13778f0b2ad0731b7e13f0ca44da7796e48f3f89
willfuentes04 <137828428+willfuentes04@users.noreply.github.com>
2024-01-17 18:59:37 -0500

    added-rating-component

---
+++ backend/data.js
@@ -0,0 +1,41 @@
+const data = {
+    products: [
+        {
+            name: "Nike shirt",
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality fabric'
+        },
+        {
+            name: "Nike Slim Pant",
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 25,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high quality fabric'
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 65,
+            countInStock: 5,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality fabric'
+        },
+    ],
+};
+export default data;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,37 @@
-
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
 function App() {
   return (
-    <div>
+  <BrowserRouter>
+    <div className='d-flex flex-column site-container'>
       <header>
-        <a href="/">sahara</a>
+        <Navbar bg="dark" variant="dark">
+          <container>
+            <LinkContainer to="/">
+            <Navbar.Brand>sahara</Navbar.Brand>
+             </LinkContainer>
+          </container>
+        </Navbar>
+        <Link to="/">sahara</Link>
+
       </header>
       <main>
-        list products
+        <Container>
+        <Routes>
+          <Route path="/product/:slug" element={<ProductScreen />} />
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+        </Container>
       </main>
+      <footer>
+        <div className='text-center'> All rights resereved</div>
+      </footer>
     </div>
+    </BrowserRouter>
   );
 }
 
